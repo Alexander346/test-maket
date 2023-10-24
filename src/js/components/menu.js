d36b1384ee573cd4ab69f9a373306ad329a0ec3a
emaile <sanya.volkov344.volkov@mail.ru>
2023-10-25 02:31:45 +0300

    examination

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -1,28 +1,40 @@
 let mainContent = document.querySelectorAll(".mainButtons");
 
 mainContent.forEach(function (button) {
-    button.addEventListener("click", function () {
-        let targetElementId = button.getAttribute("data-target");
-        let targetElement = document.getElementById(targetElementId);
+  button.addEventListener("click", function () {
+    let targetElementId = button.getAttribute("data-target");
+    let targetElement = document.getElementById(targetElementId);
+    let arrow = button.querySelector(".arrow");
 
-        if (targetElement.classList.contains("hidden")) {
+    if (targetElement.classList.contains("hidden")) {
+      closeAllElements();
 
-            closeAllElements();
-            targetElement.style.maxHeight = targetElement.scrollHeight + "px";
-            targetElement.classList.remove("hidden");
-        } else {
-            targetElement.style.maxHeight = null;
-            targetElement.classList.add("hidden");
-        }
-    });
+
+      arrow.classList.add("rotate-up");
+
+      targetElement.style.maxHeight = targetElement.scrollHeight + "px";
+      targetElement.classList.remove("hidden");
+    } else {
+
+      arrow.classList.add("rotate-down");
+
+      targetElement.style.maxHeight = null;
+      targetElement.classList.add("hidden");
+    }
+  });
 });
 
 function closeAllElements() {
-    let elements = document.querySelectorAll(".mainButtons");
-    elements.forEach(function (element) {
-        let targetId = element.getAttribute("data-target");
-        let targetElement = document.getElementById(targetId);
-        targetElement.style.maxHeight = null;
-        targetElement.classList.add("hidden");
-    });
+  let elements = document.querySelectorAll(".mainButtons");
+  elements.forEach(function (element) {
+    let targetId = element.getAttribute("data-target");
+    let targetElement = document.getElementById(targetId);
+    let arrow = element.querySelector(".arrow");
+
+    arrow.classList.remove("rotate-up");
+    arrow.classList.remove("rotate-down");
+
+    targetElement.style.maxHeight = null;
+    targetElement.classList.add("hidden");
+  });
 }
